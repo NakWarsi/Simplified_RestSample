bd565ec375825c7ec424c9bef141676348b27e9e
Naushad Warsi <warsinak@gmail.com>
2019-11-12 18:04:22 +0530

    refit Implementation Added

--- samples/WebAppUseLibrary/Controllers/HomeController.cs
+++ samples/WebAppUseLibrary/Controllers/HomeController.cs
@@ -2,23 +2,67 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
+using InterfaceLibrary;
+using InterfaceLibrary.Models;
 using Microsoft.AspNetCore.Mvc;
+using Refit;
 using WebAppUseLibrary.Models;
 
 namespace WebAppUseLibrary.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private readonly HttpClient _client;
+        private readonly IGitLibApi _restApiService;
+        public HomeController()
         {
-            return View();
+            _client = new HttpClient
+            {
+                BaseAddress = new Uri("https://api.github.com"),
+                DefaultRequestHeaders = {UserAgent = { ProductInfoHeaderValue.Parse("NakWarsi")}}
+            };
+            _restApiService = RestService.For<IGitLibApi>(_client);
+        }
+        public async Task<ViewResult> Index()
+        {
+            UserDetails userDetails;
+            try
+            {
+                // for By default 
+                //userDetails = await _restApiService.GiTHubUserDetails();
+
+                //set the value 
+                userDetails = await _restApiService.GetUser("krutikasawarkar");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+
+            return View(userDetails);
         }
 
-        public IActionResult Privacy()
+
+        public async Task<ViewResult> Accounts()
         {
-            return View();
+            List<Account> accounts;
+            try
+            {
+                //user id should be changing in positive integer value
+                accounts = await _restApiService.ListOfAccounts(0);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+            return View(accounts);
         }
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
