c7cff5bb06443818c421e25579960f71fc9aa32b
Naushad Warsi <warsinak@gmail.com>
2019-11-12 17:52:09 +0530

    Sample added using library

---
+++ samples/WebAppUseLibrary/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebAppUseLibrary.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
