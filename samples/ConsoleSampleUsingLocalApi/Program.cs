1e2835e2dead67c25a5aab633ad183930d86f7a3
Naushad Warsi <warsinak@gmail.com>
2019-11-13 11:44:48 +0530

    Add console Project

---
+++ samples/ConsoleSampleUsingLocalApi/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleSampleUsingLocalApi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
