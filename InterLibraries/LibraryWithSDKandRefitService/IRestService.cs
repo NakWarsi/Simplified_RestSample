773915a2b398efea6de7298550a7c154a859b7b8
Naushad Warsi <warsinak@gmail.com>
2019-11-13 12:08:43 +0530

    Refit Services Added

---
+++ InterLibraries/LibraryWithSDKandRefitService/IRestService.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Refit;
+
+namespace LibraryWithSDKandRefitService
+{
+    interface IRestService
+    {
+        [Get("/api/values")]
+        Task<IEnumerable<string>> GetWithNoParameter();
+
+        [Get("/api/values/{id}")]
+        Task<string> GetWithParameter([AliasAs("id")] int id);
+
+        [Post("/api/values")]
+        Task PostWithTestObject();
+
+        [Put("/api/values/{id}")]
+        Task PutWithParameters([AliasAs("id")] int id);
+
+        [Delete("/api/values/{id}")]
+        Task DeleteWithParameters([AliasAs("id")] int id);
+    }
+}
